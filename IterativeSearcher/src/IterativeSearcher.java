f57009674a026aa61d8f3349e9cc7efda407ef99
38219002 <38219002@L829WD210019.edu.cbe.ab.ca>
2025-09-17 11:49:14 -0600

    uggggggggggggggggggggggggggggh

--- IterativeSearcher/src/IterativeSearcher.java
+++ IterativeSearcher/src/IterativeSearcher.java
@@ -2,6 +2,20 @@
 public class IterativeSearcher {
     
     public int doBinarySearch(IntegerArray arr, int value) {
-        return 1;
+        int index = 0;
+        int low = 0;
+        int high = arr.length() - 1;
+        while (true) {
+            index = (int) Math.round(((double)high + (double)low)/2.0);
+            int current = arr.read(index);
+            if (current < value) {
+                low = index;
+            } else if (current == value) {
+              break;  
+            } else {
+                high = index;
+            }
+        }
+        return index;
     }
 }
