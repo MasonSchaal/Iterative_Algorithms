5aa444fcf2d964bdfe7786650b4a7f4b051667e1
38219002 <38219002@L829WD210019.edu.cbe.ab.ca>
2025-09-18 10:10:55 -0600

    done

--- IterativeMerger/src/IterativeMerger.java
+++ IterativeMerger/src/IterativeMerger.java
@@ -2,22 +2,40 @@
 public class IterativeMerger {
     
     public IntegerArray doMerge(IntegerArray a, IntegerArray b) {
-        int indexA=0, indexB=0, indexC=0;
-        IntegerArray c = new IntegerArray(a.length() + b.length());
-        while (indexA < a.length() && indexB < b.length()) {
-            if (a.read(indexA) < b.read(indexB)) {
-                c.write(indexC, a.read(indexA));
-                indexA ++;
-            } else {
-                c.write(indexB, b.read(indexB));
-                indexB ++;
+        if (a != null && b != null) {
+            int indexA=0, indexB=0, indexC=0;
+            IntegerArray c = new IntegerArray(a.length() + b.length());
+            while (indexA < a.length() && indexB < b.length()) {
+                if (a.read(indexA) < b.read(indexB)) {
+                    c.write(indexC, a.read(indexA));
+                    indexA ++;
+                } else {
+                    c.write(indexC, b.read(indexB));
+                    indexB ++;
+                }
+                indexC ++;
             }
-            indexC ++;
+            if (indexA == a.length()) {
+                for (int i = indexC ; i < c.length() ; i++) {
+                    c.write(i, b.read(indexB));
+                    indexB++;
+                }
+            } else {
+                for (int i = indexC ; i < c.length() ; i++) {
+                    c.write(i, a.read(indexA));
+                    indexA++;
+                }
+            }
+            return c;
+        } else {
+            if (a == null && b != null) {
+                return b;
+            } else if (b == null && a != null) {
+                return a;
+            } else {
+                return new IntegerArray(0);
+            }
         }
-        if (indexA == a.length()) {
-            
-        }
-        return c;
     }
 
 }
